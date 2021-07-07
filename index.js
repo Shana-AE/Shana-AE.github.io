08be6014830d288f3abaa73854747f9659e6b491
ShanaAE <Shana.wav@gmail.com>
2021-07-07 22:59:47 +0800

    test something

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log('test');
-fetch('http://shanaae.asuscomm.com:8088/test')
+fetch('https://shanaae.asuscomm.com:8888/test')
   .then((res) => res.json())
   .then((data) => console.log(data))
   .catch((err) => console.log(err));
